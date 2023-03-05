597883308da19dc94461927bd201127beddc68d5
hilmantrihatmojo <mohammed.hilmantrihatmojo@btpn.com>
2023-03-05 21:41:32 +0700

    [hilman] try add id and action listener

--- app/src/main/java/com/javierdimastri/basicandroid/MainActivity.java
+++ app/src/main/java/com/javierdimastri/basicandroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.javierdimastri.basicandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_world);
+
+        EditText editText = findViewById(R.id.editText);
+        Button editButton = findViewById(R.id.editButton);
+        TextView textView = findViewById(R.id.additionalTextView);
+
+        textView.setText("Hello");
+
+        editButton.setOnClickListener(view -> {
+            String name = editText.getText().toString();
+            textView.setText(String.format("Hello %s", name));
+        });
     }
 }
